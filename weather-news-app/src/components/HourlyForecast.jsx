d100aeb40ff8c94baca521ac34677f113a84e628
davidvillon04 <davidv04@vt.edu>
2025-04-12 13:44:55 -0400

    Update HourlyForecast.jsx

--- weather-news-app/src/components/HourlyForecast.jsx
+++ weather-news-app/src/components/HourlyForecast.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 
+import "../styles.css";
+
 const HourlyForecast = ({ hourly }) => {
    if (!hourly) return null;
 
    return (
       <div>
          <h2>Hourly Forecast (Next 24 Hours)</h2>
-         <div>
+         <div className="hourly-container">
             {hourly.slice(0, 24).map((hour, index) => (
-               <div key={index} style={{ borderBottom: "1px solid #ccc" }}>
+               <div key={index} className="hourly-item">
                   <p>
                      <strong>
                         {new Date(hour.dt * 1000).toLocaleTimeString([], {
